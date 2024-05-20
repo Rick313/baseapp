bacba1cd76e0d1cd89c99fb7a0a5843c8391b010
RAFAIDEEN Richard <richard.rafaideen@gmail.com>
2024-05-20 13:19:44 +0200

    [Misc] - Initial commit

---
+++ apps/webclient/rollup.config.js
@@ -0,0 +1,31 @@
+
+const arguments = require("minimist")(process.argv.slice(2));
+const copy = require("rollup-plugin-copy");
+const resolve = require("rollup-plugin-node-resolve");
+const serve = require("rollup-plugin-serve");
+
+/** @type {import("rollup").OutputPluginOption} */
+const plugins = [
+    resolve(),
+    copy({ targets: [
+      {src: "index.html", dest: "dist"},
+      {src: "assets", dest: "dist"}] 
+    }),
+  ];
+
+if(arguments.target == "serve") {
+  const options = { host: 'localhost', port: 4200, open: true, contentBase: ["dist"] }
+  plugins.push(serve(options));
+}
+
+/** @type {import("rollup").RollupOptions} */
+// prettier-ignore
+module.exports = [
+  { 
+    logLevel: "silent",
+    input: "src/main.js",
+    output: [{ file: "dist/main.js", format: "cjs" }],
+    plugins
+  }
+]
+
